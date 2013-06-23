6d94c0c9993e853828df5051b694d4571260670e
Edwin MacAir <edwin@thinkpixbit.com>
2013-06-23 19:50:22 -0400

    Switched $ to jQuery and removed trailing white space

--- jquery.countdown.js
+++ jquery.countdown.js
@@ -1,5 +1,5 @@
 /**
- * @author Zack Perdue / Ziggidy Creative 
+ * @author Zack Perdue / Ziggidy Creative
  * http://zackperdue.com
  * http://ziggidycreative.com
  *
@@ -10,8 +10,8 @@
  * http://www.opensource.org/licenses/mit-license.php
  *
  */
- 
- $.fn.countdown = function(options)
+
+ jQuery.fn.countdown = function(options)
 {
 	// I added this comment
 	var defaults = {
@@ -21,17 +21,17 @@
 		leadingZero: true,
 		height: 180,
 		ended: function(){
-			
+
 		}
 		height: 180
 	}
-	
+
 	var ticker_days = ['<ul>'],
 		ticker_hours = ['<ul>'],
 		ticker_minutes = ['<ul>'],
 		ticker_seconds = ['<ul>'],
 		zero = '';
-		
+
 	var now,
 		target,
 		remaining,
@@ -43,23 +43,23 @@
 		minutes,
 		a_minutes,
 		seconds;
-			
-	$.extend(defaults, options);
-	
+
+	jQuery.extend(defaults, options);
+
 	return this.each(function(){
-		
+
 		var timer = ['<ul>'];
-				
+
 		for (i=0; i < defaults.parts.length; i++){
 			timer.push('<li class="part-'+defaults.parts[i]+'"></li>');
-			
+
 			if (defaults.separator !== ":")
 			{
 				separators = (defaults.parts.length) - 0;
 			}else{
 				separators = (defaults.parts.length) - 1;
 			}
-				
+
 			if (i !== separators){
 				if(defaults.separator == ":")
 				{
@@ -72,12 +72,12 @@
 			}
 
 		}
-		
+
 		timer.push('</li>');
 		timer = timer.join('');
-		
-		$(this).append(timer);
-		
+
+		jQuery(this).append(timer);
+
 		var now = new Date(),
 			target = new Date(defaults.target),
 			remaining = target.getTime() - now.getTime(),
@@ -89,7 +89,7 @@
 			minutes = Math.floor((a_hours - hours) * 60),
 			a_minutes = (a_hours - hours) * 60,
 			seconds = Math.floor((a_minutes - minutes) * 60);
-		
+
 		for (i=0; i<=days; i++){
 			if(defaults.leadingZero){
 				if(i < 10){
@@ -99,9 +99,9 @@
 				}
 			}
 			ticker_days.push('<li>'+zero+i+'</li>');
-		} 
+		}
 		ticker_days.push('</ul>');
-		
+
 		for (i=0; i<=24; i++){
 			if(defaults.leadingZero){
 				if(i < 10){
@@ -111,9 +111,9 @@
 				}
 			}
 			ticker_hours.push('<li>'+zero+i+'</li>');
-		} 
+		}
 		ticker_hours.push('</ul>');
-		
+
 		for (i=0; i<=59; i++){
 			if(defaults.leadingZero){
 				if(i < 10){
@@ -123,9 +123,9 @@
 				}
 			}
 			ticker_minutes.push('<li>'+zero+i+'</li>');
-		} 
+		}
 		ticker_minutes.push('</ul>');
-		
+
 		for (i=0; i<=59; i++){
 			if(defaults.leadingZero){
 				if(i < 10){
@@ -135,33 +135,33 @@
 				}
 			}
 			ticker_seconds.push('<li>'+zero+i+'</li>');
-		} 
+		}
 		ticker_seconds.push('</ul>');
-		
+
 		ticker_days = ticker_days.join('');
 		ticker_hours = ticker_hours.join('');
 		ticker_minutes = ticker_minutes.join('');
 		ticker_seconds = ticker_seconds.join('');
-		
-		$(this).find('ul li.part-days').append(ticker_days);
-		$(this).find('ul li.part-hours').append(ticker_hours);
-		$(this).find('ul li.part-minutes').append(ticker_minutes);
-		$(this).find('ul li.part-seconds').append(ticker_seconds);
-		
-		var minutes_height = $('.part-minutes ul').innerHeight(),
-			days_height = $('.part-days ul').innerHeight(),
-			hours_height = $('.part-hours ul').innerHeight(),
-			seconds_height = $('.part-seconds ul').innerHeight();
-			
-		
-		$('.part-seconds ul').css({'top': (defaults.height * seconds) * -1});				
-		$('.part-minutes ul').css({'top': (defaults.height * minutes) * -1});
-		$('.part-hours ul').css({'top': (defaults.height * hours) * -1});
-		$('.part-days ul').css({'top': (defaults.height * days) * -1});
-		
-		
+
+		jQuery(this).find('ul li.part-days').append(ticker_days);
+		jQuery(this).find('ul li.part-hours').append(ticker_hours);
+		jQuery(this).find('ul li.part-minutes').append(ticker_minutes);
+		jQuery(this).find('ul li.part-seconds').append(ticker_seconds);
+
+		var minutes_height = jQuery('.part-minutes ul').innerHeight(),
+			days_height = jQuery('.part-days ul').innerHeight(),
+			hours_height = jQuery('.part-hours ul').innerHeight(),
+			seconds_height = jQuery('.part-seconds ul').innerHeight();
+
+
+		jQuery('.part-seconds ul').css({'top': (defaults.height * seconds) * -1});
+		jQuery('.part-minutes ul').css({'top': (defaults.height * minutes) * -1});
+		jQuery('.part-hours ul').css({'top': (defaults.height * hours) * -1});
+		jQuery('.part-days ul').css({'top': (defaults.height * days) * -1});
+
+
 		timer = window.setInterval(function(){
-		
+
 			var now = new Date(),
 				target = new Date(defaults.target),
 				remaining = target.getTime() - now.getTime(),
@@ -173,20 +173,20 @@
 				minutes = Math.floor((a_hours - hours) * 60),
 				a_minutes = (a_hours - hours) * 60,
 				seconds = Math.floor((a_minutes - minutes) * 60);
-			
+
 			if (remaining <= 0)
 			{
 				clearInterval(timer);
 				defaults.ended();
 			}
-				
-			$('.part-seconds ul').animate({'top': ((defaults.height * seconds) * -1)}, defaults.speed);				
-			$('.part-minutes ul').animate({'top': ((defaults.height * minutes) * -1)}, defaults.speed);
-			$('.part-hours ul').animate({'top': ((defaults.height * hours) * -1)}, defaults.speed);
-			$('.part-days ul').animate({'top': ((defaults.height * days) * -1)}, defaults.speed);		
-		
+
+			jQuery('.part-seconds ul').animate({'top': ((defaults.height * seconds) * -1)}, defaults.speed);
+			jQuery('.part-minutes ul').animate({'top': ((defaults.height * minutes) * -1)}, defaults.speed);
+			jQuery('.part-hours ul').animate({'top': ((defaults.height * hours) * -1)}, defaults.speed);
+			jQuery('.part-days ul').animate({'top': ((defaults.height * days) * -1)}, defaults.speed);
+
 		}, 1000);
-		
+
 	});
-	
+
 }
